0d7d9dd4aa9d9e245aa6eb8bdaacece07777e422
hongjie <1062480488@qq.com>
2022-10-26 15:16:14 +0800

    pytorch基本内容

--- 多层感知机/test.py
+++
@@ -1,6 +0,0 @@
-
-import torch
-from d2l import torch as d2l
-x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
-y = torch.relu(x)
-d2l.plot(x.detach(), y.detach(), 'x', 'relu(x)', figsize=(5, 2.5))
